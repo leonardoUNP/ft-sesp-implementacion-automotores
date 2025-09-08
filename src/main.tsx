0fcb09bf791ccc0b8225e6e90d3ddaeba93631d2
Leonardo <leonardo.oliveros@unp.gov.co>
2025-09-08 17:27:08 -0500

    intento shh

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "react-ecosistema-unp/utils";
 import App from "./App";
+///LOLO//
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
